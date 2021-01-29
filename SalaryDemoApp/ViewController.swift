638a2f4a7854e9a58dac0ceb8c415f63872f806d
gshukla <Gaurav.shukla@harman.com>
2021-01-29 14:10:07 +0530

    Initial Commit

---
+++ SalaryDemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SalaryDemoApp
+//
+//  Created by Shukla, Gaurav on 29/01/21.
+//  Copyright © 2021 Shukla, Gaurav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
